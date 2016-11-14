e82767496f44084594be019c9542430d98f8a371
SimeonAtcitty <atcittysimo@gmail.com>
2016-11-14 09:46:42 -0700

    added stuff

---
+++ lib/partials/carousel.php
@@ -0,0 +1,62 @@
+<div id="carousel-home" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-home" data-slide-to="1"></li>
+    <li data-target="#carousel-home" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img src="lib/images/me,Babe.jpg" alt="First slide">
+      <div class="carousel-caption">
+        <h3>Me and my Honeybun</h3>
+        <p>
+          Ava's mom and dad. Hi!
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="lib/images/meAvaNikki.jpg" alt="Second slide">
+      <div class="carousel-caption">
+        <h3>I Love My Girls.</h3>
+        <p>
+          These are my awesome baby-girls!
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="lib/images/AvaInAZ.jpg" alt="Third slide">
+      <div class="carousel-caption">
+        <h3>Happy!</h3>
+        <p>
+          She's growing so fast
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="lib/images/meBabeInAbq.jpg" alt="Third slide">
+      <div class="carousel-caption">
+        <h3>Me and Shi'Babe</h3>
+        <p>
+          Selfie at the bus stop.
+        </p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="lib/images/momAndAva.jpg" alt="Third slide">
+      <div class="carousel-caption">
+        <h3>Mommy and Ava</h3>
+        <p>
+          All dressed-up for the family reunion.
+        </p>
+      </div>
+    </div>
+
+  <a class="left carousel-control" href="#carousel-home" role="button" data-slide="prev">
+    <span class="icon-prev" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-home" role="button" data-slide="next">
+    <span class="icon-next" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
